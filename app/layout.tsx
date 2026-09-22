1d9d61474b181e673ca3300fd5949fdcf03c415f
pollo chkn <pollochkn@pollos-MacBook-Air.local>
2026-09-22 17:48:09 -0400

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-neutral-950 text-neutral-100">{children}</body>
     </html>
   );
 }
